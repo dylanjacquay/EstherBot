dd0c85667de9742012e91de9f43d26d7b1dc44e6
dylanjacquay <djacquay12@gmail.com>
2016-04-30 17:11:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('What's up, Jacob? Clearly bots are taking over the world, but you can go ahead and ask 'Will bots take over the world?' go ahead, try it.')
+            return bot.say('What's up, Jacob? Clearly bots are taking over the world, but you can go ahead and ask > Will bots take over the world? go ahead <, try it.')
                 .then(() => 'speak');
         }
     },
